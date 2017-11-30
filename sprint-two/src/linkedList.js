6a9ca1fabce8d59aa7ae40eefae303449d37d699
stphnchoe <stphnchoe@gmail.com>
2017-11-30 15:39:46 -0800

    Pass linkedlist test

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      return list.head;
+    } else {
+      var pop = list.head;
+      if (list.head.next === null) {
+        list.head = null;
+        list.tail = null;
+      } else {
+        list.head = list.head.next;
+      }
+      return pop.value;
+    }
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
