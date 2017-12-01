f22490f45b10e60e5eb5590771e3a31733dc22c0
stphnchoe <stphnchoe@gmail.com>
2017-11-30 22:17:01 -0800

    Pass binary search tree insert test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,37 @@
 var BinarySearchTree = function(value) {
+
+  var instance = Object.create(methodsBST);
+  instance.left = null;
+  instance.right = null;
+  instance.value = value;
+  return instance;
 };
 
+var methodsBST = {};
+
+methodsBST.insert = function(value) {
+  var newTree = new BinarySearchTree(value);
+  if (newTree.value > this.value) {
+    if (this.right === null) {
+      this.right = newTree;
+    } else {
+      this.right.insert(value);
+    }
+  }
+  if (newTree.value < this.value) {
+    if (this.left === null) {
+      this.left = newTree;
+    } else {
+      this.left.insert(value);
+    }
+  }
+};
+
+methodsBST.contains = function() {
+};
+
+methodsBST.depthFirstLog = function() {
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
