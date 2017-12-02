6820ac8e47edc60e0ec4fa678dcfe9e494fdaadf
stphnchoe <stphnchoe@gmail.com>
2017-12-01 16:01:32 -0800

    Pass hash table tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,50 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var pair = [k, v];
+  var limitedIndexStorage = this._storage.get(index);
+  limitedIndexStorage === undefined ? limitedIndexStorage = [] : limitedIndexStorage;
+  if (this.retrieve(k) !== undefined) {
+    for (var i = 0; i < limitedIndexStorage.length; i++) {
+      if (limitedIndexStorage[i][0] === k) {
+        limitedIndexStorage[i][1] = v;
+      }
+    }
+  } else {
+    limitedIndexStorage.push(pair);  
+    this._storage.set(index, limitedIndexStorage);
+  }
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
+//debugger;
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var limitedIndexStorage = this._storage.get(index);
+  if (limitedIndexStorage !== undefined) {
+    for (var i = 0; i < limitedIndexStorage.length; i++) {
+      if (limitedIndexStorage[i][0] === k) {
+        return limitedIndexStorage[i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var limitedIndexStorage = this._storage.get(index);
+  if (limitedIndexStorage !== undefined) {
+    for (var i = 0; i < limitedIndexStorage.length; i++) {
+      if (limitedIndexStorage[i][0] === k) {
+        limitedIndexStorage.splice(i, 1);
+      }
+    }
+  }
 };
 
 
@@ -23,4 +55,8 @@ HashTable.prototype.remove = function(k) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+
+// write test for retrieving a value with a key that is not in the hash table
+
+// refactor to search limitedIndexStorage for pair with key k
 
